5d5eb11def30d3e1611ef6962502c94c329b1622
WebDevManuel <donmanuel2005@web.de>
2023-04-27 09:51:50 +0200

    🎉 Setup project

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+//install -->import { Routes, Route } from 'react-router-dom';
+
+// components
+//import NavBar from './components/NavBar';
+
+// pages
+//import Home from './pages/Home';
+
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Appname</h1>
+            {/* <Navbar /> */}
+            {/* 
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>     
+            */}
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+const Navbar = () => {
+    return (
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+        </nav>
+    );
+}
+
+export default Navbar;
