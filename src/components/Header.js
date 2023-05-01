af14a45bf9d0ff023b2405a3dd571c2f938da988
WebDevManuel <donmanuel2005@web.de>
2023-05-01 12:54:49 +0200

    💄 Add Header.css with styles for the complete Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
+import './Header.css';
 
 function Header() {
     const [genres, setGenres] = useState([]);
@@ -17,18 +18,21 @@ function Header() {
     }
 
     return (
-        <div className='dropdown'>
-            <select className='dropdown__select' id="genre-dropdown" 
+        <Fragment>
+        <div className='header'>
+        <h1 className='header__hl'>Popular Movies</h1>
+            <select className='header__dropdown__select' id="genre-dropdown" 
             value={selectedGenreId} onChange={handleSelect}>
                 <option value="">--Select a genre--</option>
                 {genres.map(genre => (
-                    <option className='dropdown__option' key={genre.id} 
+                    <option className='header__dropdown__option' key={genre.id} 
                     value={genre.id}>
                         {genre.name}
                     </option>
                 ))}
             </select>
-        </div>
+        </div> 
+        </Fragment>
     );
 }
 
