d2b9204cd2afa3760f9ce154353b06e0cbbb4cad
supermazlum <mazlum.superc@gmail.com>
2023-05-02 20:22:00 +0200

    genre fetch solution

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -9,9 +9,35 @@ const MovieList = () => {
             .then(res => res.json())
             .then(json => {
                 setAllMovies(json.results);
-                console.log(json);
             });
-    }, [])
+    }, []);
+
+    const getGenreNames = (genreIds) => {
+        const genres = {
+            28: "Action",
+            12: "Adventure",
+            16: "Animation",
+            35: "Comedy",
+            80: "Crime",
+            99: "Documentary",
+            18: "Drama",
+            10751: "Family",
+            14: "Fantasy",
+            36: "History",
+            27: "Horror",
+            10402: "Music",
+            9648: "Mystery",
+            10749: "Romance",
+            878: "Science Fiction",
+            10770: "TV Movie",
+            53: "Thriller",
+            10752: "War",
+            37: "Western"
+        };
+
+        return genreIds.map(genreId => genres[genreId]).join(", ");
+    };
+
 
 
     return (
@@ -25,7 +51,7 @@ const MovieList = () => {
                             image={elt.poster_path}
                             year={elt.release_date}
                             // genres mappen
-                            // genre={elt.genre_ids}
+                            genre={getGenreNames(elt.genre_ids)}
                             title={elt.title}
                             overview={elt.overview}
                         />
