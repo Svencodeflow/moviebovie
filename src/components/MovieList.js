f416b0074dd0347b34e75a5fad7c80e141ab8bea
Svencodeflow <schlaakboot@gmail.com>
2023-05-02 21:18:26 +0200

    cleanWork

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -19,22 +19,6 @@ const MovieList = () => {
     const [genres, setGenres] = useState([]);
     const [selectedGenreId, setSelectedGenreId] = useState('');
 
-    // useEffect(() => {
-    //     if (message === '' && selectedGenreId === '') {
-    //         fetch('https://api.themoviedb.org/3/discover/movie?api_key=e41f6211b1b120a0d9981019e184caba&language=de_DE&sort_by=popularity.desc&include_adult=false&include_video=true&page=1&with_watch_monetization_types=flatrate')
-    //             .then(response => response.json())
-    //             .then(data => setMovies(data.results))
-    //     } else if (selectedGenreId === selectedGenreId && message === '') {
-    //         fetch(`https://api.themoviedb.org/3/discover/movie?api_key=e41f6211b1b120a0d9981019e184caba&with_genres=${selectedGenreId}&language=de_DE&sort_by=popularity.desc`)
-    //             .then(response => response.json())
-    //             .then(data => setMovies(data.results))
-    //     } else if (message === message && selectedGenreId === '') {
-    //         fetch(`https://api.themoviedb.org/3/search/movie?api_key=e41f6211b1b120a0d9981019e184caba&language=de-DE&query=${message}&page=1&include_adult=false`)
-    //             .then(response => response.json())
-    //             .then(data => setMovies(data.results))
-    //     }
-
-    // }, [message, selectedGenreId])
 
     useEffect(() => {
         if (message === '' && !selectedGenreId) {
