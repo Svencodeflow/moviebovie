a681d82c0b4e19bdfabf6ff1c269d3cf7f9ef21e
supermazlum <mazlum.superc@gmail.com>
2023-05-02 18:50:03 +0200

    style und fetch

--- src/components/MovieItems.js
+++ src/components/MovieItems.js
@@ -1,12 +1,14 @@
 const MovieItems = (props) => {
+    const year = props.year.slice(0, 4);
     return (
         <article className="movieItems">
             {/* eventuell das image als Link zu MovieDetails verlinken */}
             <p>{props.rating}</p>
-            <img src={props.image} alt="movie cover photo" />
-            <h3>{props.year} - {props.genre}</h3>
+            <img src={`https://image.tmdb.org/t/p/w500${props.image}`} alt="movie cover photo" />
+            <h3>{year} - {props.genre}</h3>
             <h2>{props.title}</h2>
         </article>
+
     );
 }
 
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,38 +1,37 @@
-import MovieItems from './MovieItems'
+import MovieItems from './MovieItems';
+import { useState, useEffect } from 'react';
+import '../css/MovieList.css';
 
 const MovieList = () => {
-    //FETCH KORRIGIEREN
-    const [allMovies, setAllMovies] = useState([])
+    const [allMovies, setAllMovies] = useState([]);
     useEffect(() => {
-        fetch(``)
+        fetch('http://api.themoviedb.org/3/movie/popular?api_key=e41f6211b1b120a0d9981019e184caba')
             .then(res => res.json())
-            .then(data => setAllMovies(data.movies))
+            .then(json => {
+                setAllMovies(json.results);
+                console.log(json);
+            });
     }, [])
 
-    return (
-        // Navbar, Header, Footer einbauen (unten)
-        <section className="movieList">
-            {/* Navbar */}
-            {/* Header */}
-            {/* Footer */}
 
+    return (
+        <section className="movieList">
             <div>
                 {allMovies.map((elt) => {
                     return (
                         <MovieItems
-                            // KEY setzen
-                            // key={elt.id}
-                            rating={elt.rating}
-                            image={elt.image}
-                            year={elt.year}
-                            // muss evtl. nochmal gemapped werden
-                            genre={elt.genre}
+                            key={elt.id}
+                            rating={elt.vote_average}
+                            image={elt.poster_path}
+                            year={elt.release_date}
+                            // genres mappen
+                            // genre={elt.genre_ids}
                             title={elt.title}
+                            overview={elt.overview}
                         />
                     )
                 })}
             </div>
-
         </section>
     );
 }
