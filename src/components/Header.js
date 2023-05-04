8deb11d49a7d2817b28e32a3e8dabb529e079f15
supermazlum <mazlum.superc@gmail.com>
2023-05-04 12:38:04 +0200

    final dropdown menu button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import '../css/Header.css';
 import Select from 'react-select';
 
 function Header(props) {
     const [genres, setGenres] = useState([]);
+    const menuPortalTarget = useRef(null);
 
     const customStyles = {
+        singleValue: (provided, state) => ({
+            ...provided,
+            color: state.isSelected ? '#8e9295' : '#8e9295',
+        }),
         control: (provided) => ({
             ...provided,
             height: '30px',
             margin: 'auto 0',
             backgroundColor: '#1A2634',
             fontFamily: 'roboto',
-            color: 'white',
+            fontSize: '1.5rem',
             borderRadius: '27px',
             borderColor: '#1A2634',
             padding: '0 10px',
-            width: '23.26vw',
+            width: '20vw',
             border: 'none',
-            borderBottom: '1px solid #ddd',
             boxShadow: 'none',
-            '&:hover': {
-                borderBottom: '1px solid #21E18C',
-            },
+            textAlign: 'left',
         }),
         indicatorSeparator: () => ({
             display: 'none',
@@ -31,24 +33,32 @@ function Header(props) {
             ...provided,
             '.myDropDown__dropdown-indicator': {
                 color: '#21E18C',
+                transition: 'transform 0.3s ease',
             },
         }),
-        dropdownIndicator: (provided) => ({
+        dropdownIndicator: (provided, state) => ({
             ...provided,
             color: '#21E18C',
+            transform: state.isFocused ? 'rotate(180deg)' : 'rotate(0deg)',
+            transition: 'transform 0.3s ease',
         }),
         option: (provided, state) => ({
             ...provided,
-            backgroundColor: state.isSelected ? '#21E18C' : 'white',
-            color: state.isSelected ? 'white' : 'black',
+            backgroundColor: state.isSelected ? '#21E18C' : '#1A2634',
+            color: state.isSelected ? 'white' : '#8e97a3',
             '&:hover': {
-                backgroundColor: state.isSelected ? '#21E18C' : '#eee',
-                color: state.isSelected ? 'white' : 'black',
+                backgroundColor: state.isSelected ? '#21E18C' : '#8e97a3',
+                color: state.isSelected ? 'white' : 'white',
             },
+            fontSize: '1.4rem',
+            textAlign: 'left',
         }),
-
+        dropdownIndicator2: (provided) => ({
+            ...provided,
+            color: '#21E18C',
+            transform: 'rotate(0deg)',
+        }),
     };
-
 
     useEffect(() => {
         fetch(
@@ -73,7 +83,9 @@ function Header(props) {
         <div className="header">
             <h1 className="header__hl">Popular Movies</h1>
             <Select
+                ref={menuPortalTarget}
                 styles={customStyles}
+                className='movieDropDown'
                 classNamePrefix="myDropDown"
                 options={options}
                 value={
